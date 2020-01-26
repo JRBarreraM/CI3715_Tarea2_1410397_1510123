7b151f1ad0975bdd67db005b9edb982a1ff6f67c
Manuel Guillermo Gil <manuelguillermogil@gmail.com>
2020-01-26 17:55:54 -0400

    Se implementó el caso para un hombre de 60 años, con 750 horas trabajadas y sin riesgos laborales

--- test.py
+++ test.py
@@ -5,7 +5,11 @@ class pensionTester(unittest.TestCase):
 
     #Mujer de 55 años con 750 horas acreditas sin años peligrosos 
     def testCase1(self):
-        self.assertEqual(aplicaPensionIVSS('F',750,'1975-01-26',0), True)
+        self.assertEqual(aplicaPensionIVSS('F',750,'1965-01-26',0), True)
+
+    #Hombre de 60 años con 750 horas acreditas sin años peligrosos 
+    def testCase4(self):
+        self.assertEqual(aplicaPensionIVSS('M',750,'1960-01-21,'0), True)
 
 if __name__ == '__main__':
     unittest.main(warnings='ignore')
