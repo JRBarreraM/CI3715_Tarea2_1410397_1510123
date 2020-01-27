4c23e77611dfb6b015a8e69c7632565b31687cd6
JRBarreraM <mrjrbarreram@gmail.com>
2020-01-27 16:47:46 -0400

    mod funcion para satisfacer case2

--- rules.py
+++ rules.py
@@ -1,6 +1,20 @@
-import datetime
+from datetime import date 
+
+#Dada una fecha de nacimiento en formato YYYY-MM-DD
+#devuelve la edad
+def calcularEdad(cumple): 
+    fechActual = date.today() 
+    edad = fechActual.year - cumple.year - ((fechActual.month, fechActual.day) < (cumple.month, cumple.day))
+    return edad
 
 #Codigo para validar si la persona aplica a una pension
 #segun lo definido en Ley del Seguro Social
 def aplicaPensionIVSS(sexo,horasAcreditadas,fechaDeNacimiento,condicionesPeligrosas):
-    return True
+    temp = fechaDeNacimiento.split('-', 3)
+    edad  = calcularEdad(date(int(temp[0]), int(temp[1]), int(temp[2])))
+
+    if sexo == 'F' and edad >= 55:
+        return True
+    elif sexo == 'M' and edad >= 60:
+        return True
+    return False
--- test.py
+++ test.py
@@ -10,6 +10,10 @@ class pensionTester(unittest.TestCase):
     #Hombre de 60 años con 750 horas acreditas sin años peligrosos 
     def testCase4(self):
         self.assertEqual(aplicaPensionIVSS('M',750,'1960-01-21',0), True)
+    
+    #Mujer de 54 años con 750 horas acreditas sin años peligrosos 
+    def testCase2(self):
+        self.assertEqual(aplicaPensionIVSS('F',750,'1966-01-26',0), False)
 
 if __name__ == '__main__':
     unittest.main(warnings='ignore')
