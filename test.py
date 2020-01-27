8cac76a06d51a64c9227dc3ce8992d66175a18ea
Manuel Guillermo Gil <manuelguillermogil@gmail.com>
2020-01-27 19:36:16 -0400

    Se implementaron casos negativos, bordes, igual a cero para fechas y horas trabajadas en peligro tanto para hombre como mujeres

--- test.py
+++ test.py
@@ -43,6 +43,95 @@ class pensionTester(unittest.TestCase):
         self.assertEqual(aplicaPensionIVSS('M',750,'1965-01-26',25), True)
         print("Case8: Approved")
 
+    #Mujer de 55 años con 749 horas acreditas sin años peligrosos 
+    def testCase9(self):
+        self.assertEqual(aplicaPensionIVSS('F',749,'1965-01-26',0), False)
+        print("Case9: Approved")
+
+    #Mujer de 55 años con 0 horas acreditas sin años peligrosos 
+    def testCase10(self):
+        self.assertEqual(aplicaPensionIVSS('F',0,'1965-01-26',0), False)
+        print("Case10: Approved")
+
+    #Hombre de 60 años con 749 horas acreditas sin años peligrosos 
+    def testCase11(self):
+        self.assertEqual(aplicaPensionIVSS('M',749,'1960-01-21',0), False)
+        print("Case11: Approved")
+
+    #Hombre de 60 años con 0 horas acreditas sin años peligrosos 
+    def testCase12(self):
+        self.assertEqual(aplicaPensionIVSS('M',0,'1960-01-21',0), False)
+        print("Case12: Approved")
+
+    #Hombre de 60 años con horas negativas acreditas sin años peligrosos 
+    def testCase13(self):
+        self.assertEqual(aplicaPensionIVSS('M',-1,'1960-01-21',0), False)
+        print("Case14: Approved")
+
+     #Mujer de 55 años con horas negativas acreditas sin años peligrosos 
+    def testCase14(self):
+        self.assertEqual(aplicaPensionIVSS('F',-1,'1965-01-26',0), False)
+        print("Case14: Approved")
+
+    #Mujer de 55 años con 749 horas acreditas con 25 años de peligro 
+    def testCase15(self):
+        self.assertEqual(aplicaPensionIVSS('F',749,'1965-01-26',25), False)
+        print("Case9: Approved")
+
+    #Mujer de 55 años con 0 horas acreditas con 25 años de peligro 
+    def testCase16(self):
+        self.assertEqual(aplicaPensionIVSS('F',0,'1965-01-26',25), False)
+        print("Case16: Approved")
+
+    #Hombre de 60 años con 749 horas acreditas con 25 años de peligro 
+    def testCase17(self):
+        self.assertEqual(aplicaPensionIVSS('M',749,'1960-01-21',25), False)
+        print("Case17: Approved")
+
+    #Hombre de 60 años con 0 horas acreditas con 25 años de peligro 
+    def testCase18(self):
+        self.assertEqual(aplicaPensionIVSS('M',0,'1960-01-21',25), False)
+        print("Case18: Approved")
+
+    #Hombre de 60 años con horas negativas acreditas con 25 años de peligro 
+    def testCase19(self):
+        self.assertEqual(aplicaPensionIVSS('M',-1,'1960-01-21',25), False)
+        print("Case19: Approved")
+
+     #Mujer de 55 años con horas negativas acreditas con 25 años de peligro 
+    def testCase20(self):
+        self.assertEqual(aplicaPensionIVSS('F',-1,'1965-01-26',25), False)
+        print("Case20: Approved")
+        #Mujer de 55 años con 749 horas acreditas con años negativo de peligro 
+    def testCase21(self):
+        self.assertEqual(aplicaPensionIVSS('F',749,'1965-01-26',-1), False)
+        print("Case21: Approved")
+
+    #Mujer de 55 años con 0 horas acreditas con años negativo de peligro 
+    def testCase22(self):
+        self.assertEqual(aplicaPensionIVSS('F',0,'1965-01-26',-1), False)
+        print("Case22: Approved")
+
+    #Hombre de 60 años con 749 horas acreditas con años negativo de peligro 
+    def testCase23(self):
+        self.assertEqual(aplicaPensionIVSS('M',749,'1960-01-21',-1), False)
+        print("Case23: Approved")
+
+    #Hombre de 60 años con 0 horas acreditas con años negativo de peligro 
+    def testCase24(self):
+        self.assertEqual(aplicaPensionIVSS('M',0,'1960-01-21',-1), False)
+        print("Case24: Approved")
+
+    #Hombre de 60 años con horas negativas acreditas con años negativo de peligro 
+    def testCase25(self):
+        self.assertEqual(aplicaPensionIVSS('M',-1,'1960-01-21',-1), False)
+        print("Case25: Approved")
+
+     #Mujer de 55 años con horas negativas acreditas con años negativo de peligro 
+    def testCase26(self):
+        self.assertEqual(aplicaPensionIVSS('F',-1,'1965-01-26',-1), False)
+        print("Case26: Approved")
+
 
 if __name__ == '__main__':
     unittest.main(warnings='ignore')
